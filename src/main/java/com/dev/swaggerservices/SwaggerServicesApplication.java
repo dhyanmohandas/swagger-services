e819316745571d9a33ebd2f653e40b6b3310d5ef
Dhyan Mohandas <dhyanmohandas@gmail.com>
2022-04-12 02:47:44 +0530

    spring boot app setup

---
+++ src/main/java/com/dev/swaggerservices/SwaggerServicesApplication.java
@@ -0,0 +1,13 @@
+package com.dev.swaggerservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerServicesApplication.class, args);
+	}
+
+}
